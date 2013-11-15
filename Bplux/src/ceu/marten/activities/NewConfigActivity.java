eff080458bdc361e2435ef7e1da35750797cf32a
carlosmarten <carlosmarten@192.168.1.100>
2013-11-16 00:47:24 +0100

    tweaked new configuration UI  + now seekbar syncs with text showing Hz  + selected channels and sensor text improved

--- Bplux/src/ceu/marten/activities/NewConfigActivity.java
+++ Bplux/src/ceu/marten/activities/NewConfigActivity.java
@@ -11,17 +11,19 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.SparseBooleanArray;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 import android.widget.Toast;
 import ceu.marten.adapters.ActiveChannelsListAdapter;
 import ceu.marten.bplux.R;
@@ -46,15 +48,43 @@ public class NewConfigActivity extends Activity {
 
 			public void onProgressChanged(SeekBar seekBar, int progress,
 					boolean fromUser) {
-				((TextView) findViewById(R.id.freq_view)).setText(String
-						.valueOf(progress + 36) + " Hz");
-				freq = progress;
+				if (fromUser) {
+					((TextView) findViewById(R.id.freq_view)).setText(String
+							.valueOf(progress + 36));
+					freq = progress;
+				}
 			}
 
 			public void onStartTrackingTouch(SeekBar seekBar) {
 			}
 
 			public void onStopTrackingTouch(SeekBar seekBar) {
+			}
+		});
+
+		final EditText freqView = (EditText) findViewById(R.id.freq_view);
+		freqView.setOnEditorActionListener(new OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView v, int actionId,
+					KeyEvent event) {
+				boolean handled = false;
+				if (actionId == EditorInfo.IME_ACTION_DONE) {
+					SeekBar frequency = (SeekBar) findViewById(R.id.freq_seekbar);
+					int new_frec = Integer.parseInt(v.getText().toString());
+					if(new_frec > 36 ){
+						frequency.setProgress((new_frec+36));
+						freq = new_frec;
+					}
+					else{
+						frequency.setProgress(0);
+						freqView.setText("36");
+						freq = 36;
+					}
+						
+					handled = true;
+				}
+				return handled;
+
 			}
 		});
 
@@ -69,23 +99,32 @@ public class NewConfigActivity extends Activity {
 		builder.setIcon(R.drawable.ic_launcher);
 		builder.setTitle("Select active channels");
 		final ArrayList<String> channels = new ArrayList<String>();
-		channels.add("channel 1");channels.add("channel 2");channels.add("channel 3");channels.add("channel 4");
-		channels.add("channel 5");channels.add("channel 6");channels.add("channel 7");channels.add("channel 8");
-		final ActiveChannelsListAdapter acla = new ActiveChannelsListAdapter(this, channels);
-		builder.setView(getLayoutInflater().inflate(R.layout.dialog_channels_listview, null));
-		builder.setPositiveButton("accept", new DialogInterface.OnClickListener() {
+		channels.add("channel 1");
+		channels.add("channel 2");
+		channels.add("channel 3");
+		channels.add("channel 4");
+		channels.add("channel 5");
+		channels.add("channel 6");
+		channels.add("channel 7");
+		channels.add("channel 8");
+		final ActiveChannelsListAdapter acla = new ActiveChannelsListAdapter(
+				this, channels);
+		builder.setView(getLayoutInflater().inflate(
+				R.layout.dialog_channels_listview, null));
+		builder.setPositiveButton("accept",
+				new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
-						
-		                TextView ca = (TextView) findViewById(R.id.txt_canales_activos);
-		                ca.setText("selected channels: ");
-		                String[] checked = acla.getChecked();
-		                String si= "";
-		                for (int i = 0; i < checked.length; i++) {
-		                    if (checked[i]!=null)
-		                        si=si+(i+1)+","+checked[i]+";";
-		                }
-		                ca.append(si);
+
+						TextView ca = (TextView) findViewById(R.id.txt_canales_activos);
+						ca.setText("selected channels: \n\n");
+						String[] checked = acla.getChecked();
+						String si = "";
+						for (int i = 0; i < checked.length; i++) {
+							if (checked[i] != null)
+								si = si+"\t channel " + (i + 1) + " with sensor " + checked[i] + "\n";
+						}
+						ca.append(si);
 					}
 				});
 		builder.setNegativeButton("cancel",
@@ -96,16 +135,16 @@ public class NewConfigActivity extends Activity {
 						dialog.dismiss();
 					}
 				});
-		
+
 		AlertDialog ad = builder.create();
 		ad.show();
-		
-		lv =(ListView) ad.findViewById(R.id.lv_channelsSelection);
+
+		lv = (ListView) ad.findViewById(R.id.lv_channelsSelection);
 		lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 		lv.setItemsCanFocus(false);
-		
+
 		lv.setAdapter(acla);
-		
+
 	}
 
 	@Override
